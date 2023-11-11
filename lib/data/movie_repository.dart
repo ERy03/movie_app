f25023526eb59a18efe2ba2f33856cf9c7c87e44
Ryota <ryoenoki2013@gmail.com>
2023-11-11 12:56:36 +0100

    update: use env

--- lib/data/movie_repository.dart
+++ lib/data/movie_repository.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:movie_app/domain/models/movie.dart';
 import 'package:movie_app/domain/models/movies_response.dart';
+import 'package:movie_app/env/env.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'movie_repository.g.dart';
@@ -14,7 +13,7 @@ class MovieRepository {
 
   Future<List<Movie>> fetchPopularMovies() async {
     final response = await dio.get(
-      'https://api.themoviedb.org/3/movie/popular?api_key=ce83de2f2472debe94b0907148d9b12d',
+      'https://api.themoviedb.org/3/movie/popular?api_key=${Env.apiKey}',
     );
     final movies = MoviesResponse.fromMap(response.data);
     print(movies);
